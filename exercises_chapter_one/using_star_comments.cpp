34258fe3dbe42eeadb5ed27145d33ecf2f2b538b
punya <punyavashist@pm.me>
2018-06-17 09:00:00 -0700

    Added star delimiters program

---
+++ exercises_chapter_one/using_star_comments.cpp
@@ -0,0 +1,9 @@
+//Ah, so the program below does not work because star delimiters can't be used inside an iteration of themselves.
+#include <iostream>
+	int main() {
+		/* I call comments like these star comments for reasons I really can't point back to.
+			Maybe because these comments have the power to span a couple lines.
+				Or maybe because they use /* and */ as the starting and ending delimiters */
+		std::cout << "Does this work? Of course it does!" << std::endl;
+			return 0;
+	}
