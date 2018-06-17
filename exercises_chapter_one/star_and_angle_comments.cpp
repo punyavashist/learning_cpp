564ad88bfb268f19b4bd6351df33608a7075b49f
punya <punyavashist@pm.me>
2018-06-17 09:00:00 -0700

    This is a bit strange

---
+++ exercises_chapter_one/star_and_angle_comments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+	int main() {
+		// This is a comment that extends over several lines
+		//by using // at the beginning of each line instead of using /*
+			// or */ to delimit comments.
+		std::cout << "Does this work, now?" << std::endl;
+			return 0;
+		} 
+
+	
+	//Oh, so star delimiters can be used in this way ^. Hmmmmm.
